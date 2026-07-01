1af450400de6e3962b9655e6118ce4a187b9b1c3
Paul <198077751+paulmwash@users.noreply.github.com>
2026-07-01 10:53:24 +0300

    Update moseh.c

--- moseh.c
+++ moseh.c
@@ -1,3 +1,10 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
 
+void id(int argc, char *argv[]){
+  if (argc ==1)
+    printf("Please enter your name");
+  else{
+    printf("Your name is %s",argv[1]);
+  }  
